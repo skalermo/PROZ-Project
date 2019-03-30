113b08bfd885f8243cbe920ffe19598c8907bb52
skalermo <romnmko@gmail.com>
2019-03-30 17:09:11 +0100

    Created start menu prototype

---
+++ src/model/MenuButton.java
@@ -0,0 +1,106 @@
+package model;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Font;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class MenuButton extends Button {
+
+    private final String FONT_PATH = "src/model/resources/some_font.ttf";
+    private final String BUTTON_PRESSED_STYLE = "-fx-background-color: rgba(0, 100, 50, 1)";
+    private final String BUTTON_RELEASED_STYLE = "-fx-background-color: rgba(0, 100, 50, 1)";
+    private final String BUTTON_ENTERED_STYLE = "-fx-background-color: rgba(0, 100, 50, 1)";
+    private final String BUTTON_EXITED_STYLE = "-fx-background-color: rgba(0, 100, 50, 0.5)";
+
+    public MenuButton(String text) {
+        setText(text);
+        setButtonFont();
+        setPrefWidth(190);
+        setPrefHeight(49);
+        setStyle(BUTTON_EXITED_STYLE);
+        initializeButtonListeners();
+    }
+
+    private void setButtonFont() {
+
+        try {
+            setFont(Font.loadFont(new FileInputStream(FONT_PATH), 23));
+        } catch (FileNotFoundException e) {
+            setFont(Font.font("Verdana", 23));
+        }
+
+    }
+
+    private void setButtonPressedStyle() {
+
+        setStyle(BUTTON_PRESSED_STYLE);
+        setPrefHeight(45);
+        setLayoutY(getLayoutY() + 4);
+    }
+
+    private void setButtonRealesedStyle() {
+
+        setStyle(BUTTON_RELEASED_STYLE);
+        setPrefHeight(49);
+        setLayoutY(getLayoutY() - 4);
+    }
+
+    private void setButtonEnteredStyle() {
+
+        setStyle(BUTTON_ENTERED_STYLE);
+        setPrefHeight(49);
+        setLayoutY(getLayoutY());
+    }
+
+    private void setButtonExitedStyle() {
+
+        setStyle(BUTTON_EXITED_STYLE);
+        setPrefHeight(49);
+        setLayoutY(getLayoutY());
+    }
+
+    private void initializeButtonListeners() {
+
+        setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
+                    setButtonPressedStyle();
+                }
+            }
+        });
+
+        setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
+                    setButtonRealesedStyle();
+                }
+            }
+        });
+
+        setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                setButtonEnteredStyle();
+                setEffect(new DropShadow());
+            }
+        });
+
+
+        setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                setButtonExitedStyle();
+                setEffect(null);
+            }
+        });
+    }
+
+}
--- src/view/ViewManager.java
+++ src/view/ViewManager.java
@@ -1,27 +1,128 @@
 package view;
 
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import model.MenuButton;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ViewManager {
 
-    public static final int WIDTH = 800;
-    public static final int HEIGHT = 600;
+    private static Logger log = LogManager.getRootLogger();
+
     private AnchorPane mainPane;
-    private Scene mainScene;
+    private Scene startMenuScene;
+
+    private AnchorPane gamePane;
+    private Scene gameScene;
+
     private Stage mainStage;
+    private static final int SCR_WIDTH = 1200;
+    private static final int SCR_HEIGHT = 800;
+
+    private static final int MENU_BUTTONS_START_X = (SCR_WIDTH-190)/2;
+    private static final int MENU_BUTTONS_START_Y = 150;
+
+    List<MenuButton> startMenuButtons;
+
 
 
     public ViewManager() {
+        startMenuButtons = new ArrayList<>();
         mainPane = new AnchorPane();
-        mainScene = new Scene(mainPane, WIDTH, HEIGHT);
+
+        startMenuScene = new Scene(mainPane, SCR_WIDTH, SCR_HEIGHT);
+
+        gamePane = new AnchorPane();
+        gameScene = new Scene(gamePane, SCR_WIDTH, SCR_HEIGHT);
+
+
         mainStage = new Stage();
-        mainStage.setScene(mainScene);
+        mainStage.setScene(startMenuScene);
+
+        createButtons();
+//        createBackground();
     }
 
     public Stage getMainStage() {
         return mainStage;
+    }
+
+    private void addMenuButton(MenuButton button) {
+
+        button.setLayoutX(MENU_BUTTONS_START_X);
+        button.setLayoutY(MENU_BUTTONS_START_Y + startMenuButtons.size() * 100);
+        startMenuButtons.add(button);
+        mainPane.getChildren().add(button);
+
+    }
+
+    private void createButtons() {
+
+        createNewGameButton();
+        createScoresButton();
+        createHelpButton();
+        createCreditsButton();
+        createExitButton();
+    }
+
+    private void createNewGameButton() {
+
+        MenuButton newGameButton = new MenuButton("New game");
+        addMenuButton(newGameButton);
+
+        newGameButton.setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                mainStage.setScene(gameScene);
+            }
+        });
+    }
+
+    private void createScoresButton() {
+
+        MenuButton scoresButton = new MenuButton("Scores");
+        addMenuButton(scoresButton);
+    }
+
+    private void createHelpButton() {
+
+        MenuButton helpButton = new MenuButton("Help");
+        addMenuButton(helpButton);
+    }
+
+    private void createCreditsButton() {
+
+        MenuButton creditsButton = new MenuButton("Credits");
+        addMenuButton(creditsButton);
+    }
+
+    private void createExitButton() {
+
+        MenuButton exitButton = new MenuButton("Exit");
+        addMenuButton(exitButton);
+
+        exitButton.setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                mainStage.close();
+            }
+        });
+
+
+    }
+
+    private void createBackground() {
+        Image backgroundImage = new Image("/view/resources/menubgfull.png", 1920, 1080, false, true);
+        BackgroundImage background  = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, null);
+        mainPane.setBackground(new Background(background));
     }
 }
